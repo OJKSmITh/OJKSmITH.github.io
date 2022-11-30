4a9f435d4966532f439055c85c00ee8b1da3c888
hj <rupan0545@naver.com>
2022-11-30 12:35:07 +0900

    slide 구현

--- JS/header.js
+++ JS/header.js
@@ -6,3 +6,28 @@ toggleBtn.addEventListener("click", () => {
     menu.classList.toggle("active")
     icons.classList.toggle("active")
 })
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+

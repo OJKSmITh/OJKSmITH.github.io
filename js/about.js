586c09c6bc7e027b3e9140fb565c69993d2d78f9
hj <rupan0545@naver.com>
2022-12-02 17:48:03 +0900

    진짜 마지막

---
+++ js/about.js
@@ -0,0 +1,49 @@
+let popup = document.querySelector("#popup")
+function openPopup() {
+    popup.classList.add("open-popup")
+}
+
+function closePopup() {
+    popup.classList.remove("open-popup")
+}
+
+let popup2 = document.querySelector("#popup2")
+function openPopup2() {
+    popup2.classList.add("open-popup")
+}
+
+function closePopup2() {
+    popup2.classList.remove("open-popup")
+}
+
+let popup3 = document.querySelector("#popup3")
+function openPopup3() {
+    popup3.classList.add("open-popup")
+}
+
+function closePopup3() {
+    popup3.classList.remove("open-popup")
+}
+
+
+let popup4 = document.querySelector("#popup4")
+function openPopup4() {
+    popup4.classList.add("open-popup")
+}
+
+function closePopup4() {
+    popup4.classList.remove("open-popup")
+}
+
+let popup5 = document.querySelector("#popup5")
+
+function openPopup5() {
+    popup5.classList.add("open-popup")
+}
+
+function closePopup5() {
+    popup5.classList.remove("open-popup")
+}
+
+
+
